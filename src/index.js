8fc4d9558290c7f070bd3783dd68d508eb68025b
Altynay Umpi <zm412412@gmail.com>
2020-07-16 18:04:15 -0400

    Added loading css and js in server

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ let app = express();
 
 app.set('view engine', 'ejs');
 
+app.use('/public', express.static('public'));
+
 app.get(/\/*/, function (req, res){
   let name = req.url.slice(1);
     res.render(name);
 });
+
+
 
 
 app.listen(8888);
