bf8e04e71e3904844efeecb4dbbd551dab157a7d
Altynay Umpi <zm412412@gmail.com>
2020-08-12 19:40:14 -0400

    Added connection with mongoDb, page about

--- src/index.js
+++ src/index.js
@@ -1,10 +1,54 @@
 
+require('dotenv').config();
 let fs = require('fs');
 let express = require('express');
 let bodyParser = require('body-parser');
 let app = express();
-
 let urlencodedParser = bodyParser.urlencoded({ extended: false });
+let mongoose = require('mongoose');
+
+async function start(){
+  try{
+    await mongoose.connect(process.env.MONGO_URI,{ 
+              useNewUrlParser: true,
+              useFindAndModify: false,
+              useUnifiedTopology: true 
+            })
+
+    app.listen(8888, function(){
+        console.log("Сервер ожидает подключения...");
+    });
+
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+
+start();
+
+let Schema = mongoose.Schema;
+const aboutSchema = new Schema({
+  email: {
+    type: String,
+    //match: /^.+@.+\..+$/igm,
+    require: true
+  },
+  check:{
+    type: String,
+    default: 'off'
+  } ,
+  moreInfo: {
+    type: String,
+    default: 'off'
+  },
+  date: {
+    type: Date,
+    default: Date.now 
+  }
+}, { versionKey: false });
+
+const Adress = mongoose.model('Adress', aboutSchema);
 
 app.set('view engine', 'ejs');
 
@@ -12,12 +56,24 @@ app.use('/public', express.static('public'));
 
 app.post('/about', urlencodedParser, function (req, res) {
   if(!req.body) return res.sendStatus(400);
+  let adressIs;
   
   
-  console.log(req.body);
-  res.render('about-success', {
-    title: 'about-success',
-    data: req.body});
+  Adress.find({email: req.body.email}, (err, doc) => {
+    if(err) console.log(err);
+    if(doc.length == 0){
+      adressIs = 'Ваш адрес только что внесен в базу данных MongoDb для ежегодной новостной рассылки. Поздравляем!!!'; 
+      Adress.create({email: req.body.email, check: req.body.check, moreInfo: req.body.moreInfo}, (err, doc) => {});
+    }else{
+      adressIs = "Ваш электронный адрес (" + req.body.email + ") уже есть в списке на ежегодную рассылку! Подписка произведена " + doc[0].date; 
+    }
+
+          res.render('about-success', {
+            title: 'about-success',
+            data: req.body,
+            adressIs: adressIs
+          });
+  })
 });
 
 app.get(/\/*/, function (req, res){
@@ -38,7 +94,6 @@ app.get(/\/*/, function (req, res){
 
 
 
-app.listen(8888);
 
 
 
--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,7 +1,39 @@
-'use strict'
+
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state = { nums: [1,2,3,4,5,6,7,8,9], numForDel:0 };
+    }
+
+  deleteEl(num){
+  }
+
+  takeIndex(event){
+    this.state.numForDel = Number(event.target.value);
+    this.setState({numForDel: this.state.numForDel});
+  }
+
+  startAction(event){
+    event.preventDefault();
+    this.state.nums.splice(this.state.numForDel, 1);
+    this.setState({nums:this.state.nums}) ;
+  }
+
+  render(){
+    let list = this.state.nums.map((item, index) =>{
+      return <li key={index}>{item}</li>
+    })
+
+    return <div>
+  <ul>{list}</ul>
+  <form onSubmit={this.startAction.bind(this)}>
+    <input id="" type="text" onChange={this.takeIndex.bind(this)} />
+    <input type="submit" value="" />
+  </form>
+</div>
+  }
 
 
-let myGame = require('./gameX0.js')
-myGame.createGame();
-myGame.startGame();
+}
 
+ReactDOM.render(<App />, document.getElementById('contentIndex'))
