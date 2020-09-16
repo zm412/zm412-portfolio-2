589231365e75ce64b6d05370c5d9e5f2681d5ef4
zm412 <zm412412@gmail.com>
2020-09-16 17:12:00 -0400

    fixed sortByClick

--- src/js/tasks/sortByClick.js
+++ src/js/tasks/sortByClick.js
@@ -4,16 +4,25 @@ module.exports = () => {
 
 let blockSortBy = document.getElementById('sortByClick');
 
-  function getTable(){
-blockSortBy.insertAdjacentHTML('afterbegin', `
-  		<table id = 'tblSortByClick' class = 'middle table table-bordered'>
-		<tr class = 'trSort'><th class = 'thSort'>Name</th><th class = 'thSort'>dateOfSomething</th><th class = 'thSort'>Plants</th><th class = 'thSort'>num</th></tr>
-		<tr class = 'trSort'><td class = 'tdSort'>Mila</td><td class = 'tdSort'>10/18/2002</td><td class = 'tdSort'>mango</td><td class = 'tdSort'>23</td></tr>
-		<tr class = 'trSort'><td class = 'tdSort'>Ruslan</td><td class = 'tdSort'>09/25/1999</td><td class = 'tdSort'>orange</td><td class = 'tdSort'>15</td></tr>
-		<tr class = 'trSort'><td class = 'tdSort'>James</td><td class = 'tdSort'>12/19/1998</td><td class = 'tdSort'>apple</td><td class = 'tdSort'>55</td></tr>
-		<tr class = 'trSort'><td class = 'tdSort'>Karina</td><td class = 'tdSort'>04/17/1999</td><td class = 'tdSort'>carrot</td><td class = 'tdSort'>88</td></tr>
-		<tr class = 'trSort'><td class = 'tdSort'>Tanya</td><td class = 'tdSort'>11/15/1996</td><td class = 'tdSort'>peach</td><td class = 'tdSort'>13</td></tr>
-    <tr class = 'trSort'><td class = 'tdSort'>Sofia</td><td class = 'tdSort'>05/15/2001</td><td class = 'tdSort'>yukka</td><td class = 'tdSort'>25</td></tr>
+if(blockSortBy != null){
+      getTableSortByClick(blockSortBy);
+      startSortByClick();
+}
+
+}
+
+
+
+  function getTableSortByClick(elem){
+elem.insertAdjacentHTML('afterbegin', `
+  		<table id='tblSortByClick' class='middle table table-bordered'>
+		<tr class='trSort'><th class='thSort'>SortByName</th><th class='thSort'>SortByDate</th><th class='thSort'>BackSort</th><th class='thSort'>SortNumbers</th></tr>
+		<tr class='trSort'><td class='tdSort'>Mila</td><td class='tdSort'>10/18/2002</td><td class='tdSort'>mango</td><td class='tdSort'>23</td></tr>
+		<tr class='trSort'><td class='tdSort'>Ruslan</td><td class='tdSort'>09/25/1999</td><td class='tdSort'>orange</td><td class='tdSort'>15</td></tr>
+		<tr class='trSort'><td class='tdSort'>James</td><td class='tdSort'>12/19/1998</td><td class='tdSort'>apple</td><td class='tdSort'>55</td></tr>
+		<tr class='trSort'><td class='tdSort'>Karina</td><td class='tdSort'>04/17/1999</td><td class='tdSort'>carrot</td><td class='tdSort'>88</td></tr>
+		<tr class='trSort'><td class='tdSort'>Tanya</td><td class='tdSort'>11/15/1996</td><td class='tdSort'>peach</td><td class='tdSort'>13</td></tr>
+    <tr class='trSort'><td class='tdSort'>Sofia</td><td class='tdSort'>05/15/2001</td><td class='tdSort'>yukka</td><td class='tdSort'>25</td></tr>
 			</table>
 `)
 
@@ -23,40 +32,47 @@ blockSortBy.insertAdjacentHTML('afterbegin', `
 
 
 function startSortByClick(){
-  let tds = document.querySelectorAll('.tdSort');
   let trs = document.querySelectorAll('.trSort');
   let ths = trs[0].querySelectorAll('.thSort');
 
 	ths[0].addEventListener('click', function(){
-	putActionsTogether(sortByName);			
+	putActionsTogether(sortByName, ths[0].innerHTML);			
 		});
 
 	ths[2].addEventListener('click', function(){
-	putActionsTogether(simpleSort);			
+	putActionsTogether(backSort, ths[2].innerHTML);			
 		});
 	
 	ths[3].addEventListener('click', function(){
-	putActionsTogether(compareNumbers);	
+	putActionsTogether(compareNumbers, ths[3].innerHTML);	
 	});
 
 	ths[1].addEventListener('click', function(){
-	putActionsTogether(compareDates);	
+	putActionsTogether(compareDates, ths[1].innerHTML);	
 	});
 	
-                function putActionsTogether(func){
-                    let comlpArr = createObjFromInners();
-                    let newComplArr = func(comlpArr);
+
+
+}
+
+
+                function putActionsTogether(func, key){
+                  let tds = document.querySelectorAll('.tdSort');
+                  let trs = document.querySelectorAll('.trSort');
+                  let ths = trs[0].querySelectorAll('.thSort');
+                    let comlpArr = createObjFromInners(trs, ths);
+                    let newComplArr = func(comlpArr, key);
                     let changedNewComplArr = getSimpleArr(newComplArr);
-                    putArrInTbl(changedNewComplArr);
+                    putArrInTbl(changedNewComplArr, tds);
                 }	
 
-                function createObjFromInners(){
+                function createObjFromInners(trsElem, thsElem){
                   let infFromTbl = [];
-                  for(let i = 1; i < trs.length; i++){
-                    let tds = trs[i].querySelectorAll('td');
+                  for(let i = 1; i < trsElem.length; i++){
+                    let tdsElem = trsElem[i].querySelectorAll('td');
                     let oneLine = {};
-                    for(let j = 0; j < ths.length; j++){
-                    oneLine[ths[j].innerHTML] = tds[j].innerHTML;
+                    for(let j = 0; j < thsElem.length; j++){
+                    oneLine[thsElem[j].innerHTML] = tdsElem[j].innerHTML;
                   }
                     infFromTbl.push(oneLine);
                 }
@@ -75,68 +91,46 @@ function startSortByClick(){
                 }
                     
 
-                function putArrInTbl(arr){
-                  for(let i = 0; i < tds.length; i++){
-                    tds[i].innerHTML = arr[i];
+                function putArrInTbl(arr, tdsElem){
+                  for(let i = 0; i < tdsElem.length; i++){
+                    tdsElem[i].innerHTML = arr[i];
                   }}
 
 
 
-                function sortByName(arr){
+                function sortByName(arr, key){
                   arr.sort(function(a, b){
-                  let nameA = a.Name.toLowerCase();
-                  let nameB = b.Name.toLowerCase();
-                  if(nameA < nameB)
-                    return -1;
-                  
-                  if(nameA > nameB)
-                    return 1;
-                      
-                    return 0; 
+                      let nameA = a[key].toLowerCase();
+                      let nameB = b[key].toLowerCase();
+                      if(nameA < nameB) return -1;
+                      if(nameA > nameB) return 1;
+                      return 0; 
                 });
                     return arr;
                 }
+ 
+
+                function backSort(arr, key){
+                  return sortByName(arr, key).reverse();
+               }
 
 
-                function compareDates(arr){
+
+                function compareDates(arr, key){
                   arr.sort(function(a,b){
-                  let dateA = new Date (a.dateOfSomething);
-                  let dateB = new Date(b.dateOfSomething);
+                  let dateA = new Date (a[key]);
+                  let dateB = new Date(b[key]);
                   return dateA - dateB; 
                   });
                     return arr;
                 }
                   
-                function compareNumbers(arr){
+                function compareNumbers(arr, key){
                   arr.sort(function(a,b){
-                  return a.num - b.num;
+                    return a[key] - b[key];
                   });
                     return arr;
                 }
-                  
-                function simpleSort(arr){
-                  arr.sort(function(a, b){
-                  let nameA = a.Plants.toLowerCase();
-                  let nameB = b.Plants.toLowerCase();
-                  if(nameA < nameB)
-                    return -1;
-                  
-                  if(nameA > nameB)
-                    return 1;
-                      
-                    return 0; 
-                });
-                    return arr;
-                }
+                 
 
 
-
-}
-
-
-if(blockSortBy != null){
-      getTable();
-      startSortByClick();
-}
-
-}
