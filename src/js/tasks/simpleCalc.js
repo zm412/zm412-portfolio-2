30ae69b20f8df38b57c902a3daa23afff7611b0d
zm412 <zm412412@gmail.com>
2020-09-17 11:52:53 -0400

    fixed simpleCalc

--- src/js/tasks/simpleCalc.js
+++ src/js/tasks/simpleCalc.js
@@ -4,9 +4,17 @@ module.exports = () => {
 
 let simpleCalc = document.getElementById('simpleCalc');
 let collector;
+if(simpleCalc != null){
+	let collector = '';
+  getCalc(simpleCalc);
+  getHigthFromTop(collector);
+}
+}
 
-function getCalc(){
-  simpleCalc.insertAdjacentHTML('afterbegin', `
+
+
+function getCalc(elem){
+  elem.insertAdjacentHTML('afterbegin', `
     <table id = 'tblCalc'>
       <tr>
         <th>C</th>
@@ -19,7 +27,7 @@ function getCalc(){
   }
 
 
-function getHigthFromTop(){
+function getHigthFromTop(collectorVar){
 	let inp = document.getElementById('inpCalc');			
 	let p = document.getElementById('infoCalc');			
 	let tbl = document.getElementById('tblCalc');
@@ -27,85 +35,72 @@ function getHigthFromTop(){
 	let symbols = [7,8,9,'/',4,5,6,'*', 1,2,3,'-', 0, '.', '=', '+', 'Restart', 'BS', 'Insert',''];			
 		
 	getTbl(tbl, symbols);
-	getTargetClick('td', inp, p);
-
-	
-                function deleteOfLastElem(elem){
-                    let arr = elem.innerHTML.split('');
-                    arr.pop();
-                    elem.innerHTML = arr.join('');
-                }
-  
-                function createEl(tag, par){		
-                  let el = document.createElement(tag);	
-                    par.appendChild(el);
-                    return el;
-                }		
-
-
-                function getTbl(elem, arr){				
-                  let k = 0;			
-                    for(let i = 1; i <= 5; i++){		
-                      let tr = createEl('tr', elem);	
-                      for(let j = 1; j <= 4; j++){	
-                        let td = createEl('td', tr);
-                        td.innerHTML = arr[k];		
-                        td.className = 'tdCalc';
-                        k++;		
-                      }			
-                    }				
-                }						
-
-                function getTargetClick(tag, elInp, elP){						
-                  let elems = document.querySelectorAll('.tdCalc');				
-
-                  for(let i = 0;  i < elems.length; i++){				
-                    elems[i].addEventListener('click', function getExc(){		
-                      let inInp = this.innerHTML;
-
-                    if(inInp >= 0 || inInp == '.'){	
-                      elInp.value += inInp;		
-                      elP.innerHTML += inInp;
-                    }
-
-                    else if(inInp == '/' || inInp == '*' || inInp == '+' || inInp == '-'){
-                      elInp.value += inInp;		
-                      elP.innerHTML += inInp;
-                      elInp.value = '';
-
-                    }else if(inInp == '='){	
-                      collector = eval(elP.innerHTML);
-                      elInp.value = eval(elP.innerHTML);
-                      elP.innerHTML += ' = ' + elInp.value;
-                      
-                    }else if(inInp == 'BS'){
-                      let tempa = elInp.value.split('');
-                      tempa.pop();
-                      elInp.value = tempa.join('');
-                      let anotherTempa = elP.innerHTML.split('');
-                      anotherTempa.pop();
-                      elP.innerHTML = anotherTempa.join('');
-
-                    }else if(inInp == 'Insert'){
-                      elInp.value += collector;
-
-                    }else if(inInp == 'Restart'){
-                      elP.innerHTML = '';
-                      elInp.value = '';
-                    };
-                          
-                      });
-                    }	
-                }			
-
-
-}
-
-if(simpleCalc != null){
-	let collector = '';
-  getCalc();
-  getHigthFromTop();
+	getTargetClick('td', inp, p, collectorVar);
 }
 
 
+
+
+function deleteOfLastElem(elem){
+    let arr = elem.innerHTML.split('');
+    arr.pop();
+    elem.innerHTML = arr.join('');
 }
+
+function createEl(tag, par){		
+  let el = document.createElement(tag);	
+    par.appendChild(el);
+    return el;
+}		
+
+
+function getTbl(elem, arr){				
+  console.log('hi')
+  let k = 0;			
+    for(let i = 1; i <= 5; i++){		
+      let tr = createEl('tr', elem);	
+      for(let j = 1; j <= 4; j++){	
+        let td = createEl('td', tr);
+        td.innerHTML = arr[k];		
+        td.className = 'tdCalc';
+        k++;		
+      }			
+    }				
+}						
+
+function getTargetClick(tag, elInp, elP, collectorVar){						
+  let elems = document.querySelectorAll('.tdCalc');				
+  for(let i = 0;  i < elems.length; i++){				
+    elems[i].addEventListener('click', function getExc(){		
+      let inInp = this.innerHTML;
+    if(inInp >= 0 || inInp == '.'){	
+      elInp.value += inInp;		
+      elP.innerHTML += inInp;
+    }else if(inInp == '/' || inInp == '*' || inInp == '+' || inInp == '-'){
+      elInp.value += inInp;		
+      elP.innerHTML += inInp;
+      elInp.value = '';
+    }else if(inInp == '='){	
+      collectorVar = eval(elP.innerHTML);
+      elInp.value = eval(elP.innerHTML);
+      elP.innerHTML += ' = ' + elInp.value;
+    }else if(inInp == 'BS'){
+      afterBS(elInp, elP); 
+    }else if(inInp == 'Insert'){
+      elInp.value += collectorVar;
+    }else if(inInp == 'Restart'){
+      elP.innerHTML = '';
+      elInp.value = '';
+    }; }); }	
+}			
+
+function afterBS(elInp, elP){
+    let tempa = elInp.value.split('');
+      tempa.pop();
+      elInp.value = tempa.join('');
+      let anotherTempa = elP.innerHTML.split('');
+      anotherTempa.pop();
+      elP.innerHTML = anotherTempa.join('');
+}
+
+
