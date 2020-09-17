e95ec0b86076555776e1287e9d1539fe2cd49edd
zm412 <zm412412@gmail.com>
2020-09-17 15:32:49 -0400

    new changebls

--- src/js/tasks/simpleCalc.js
+++ src/js/tasks/simpleCalc.js
@@ -70,7 +70,6 @@ function getTbl(elem, arr){
 
 function getTargetClick(elInp, elP, collectorVar){						
   let allSymbols = [
-<<<<<<< HEAD
     { expr: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'],
       func: (exp) => { elInp.value += exp;		
                        elP.innerHTML += exp; } },
@@ -97,59 +96,6 @@ function getTargetClick(elInp, elP, collectorVar){
             if(arr.expr.includes(inInp)) arr.func(inInp);
         });
       } } } 
-=======
-    {
-      expr: ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'],
-      func: (exp) => {
-            elInp.value += exp;		
-            elP.innerHTML += exp; }
-    },
-    {
-      expr: ['/', '*', '+', '-'],
-      func: (exp) => {
-            elInp.value += exp;		
-            elP.innerHTML += exp;
-            elInp.value = ''; }
-    },
-    {
-      expr: ['='],
-      func: (exp) => {
-            collectorVar = eval(elP.innerHTML);
-            elInp.value = eval(elP.innerHTML);
-            elP.innerHTML += ' = ' + elInp.value; }
-    },
-    {
-      expr: ['BS'],
-      func: (exp) => { afterBS(elInp, elP); }
-    },
-    {
-      expr: ['Insert'],
-      func: (exp) => { elInp.value += collectorVar; }
-    },
-    {
-      expr: ['Restart'],
-      func: (exp) => { elP.innerHTML = ''; elInp.value = ''; }
-    }
-  ];
-
-   let elems = document.querySelectorAll('.tdCalc');
-
-    for(let arr of allSymbols){
-     for(let i = 0;  i < elems.length; i++){
-        elems[i].addEventListener('click', function getExc(){
-        let inInp = elems[i].innerHTML;
-            if(arr.expr.includes(inInp)){
-              arr.func(inInp);
-            }
-        });
-      }
-   }
-}
-
->>>>>>> 36011139387ee302ea59687e735b60d5f347aabd
-
-
-
 
 
 function afterBS(elInp, elP){
