3b39bd36953c4c675cc7f8133eae03b35cdc4258
zm412 <zm412412@gmail.com>
2020-09-16 20:18:24 -0400

    fixed tableTranspose

--- src/js/tasks/tableTranspose.js
+++ src/js/tasks/tableTranspose.js
@@ -3,21 +3,25 @@
 module.exports = () => {
 
   let transpose = document.getElementById('transpose');
+  if(transpose != null){
+      getTableTranspose(transpose);
+      startActionTranspose();
+  }
+}
 
-  function getTable(){
 
-  transpose.insertAdjacentHTML('afterbegin', `
-		<input id = 'rowsTableTranspose' placeholder = 'int <= 15'><br>
-		<input id = 'columnsTableTranspose' placeholder = 'int <= 15'><br>
-		<div id = 'elemTableTranspose'>
-      
-				<table id = 'transposeTbl' class = 'middle '></table></div><br>
 
-    <p id = 'errTableTranspose'></p>
-    <button id = 'buttCreateTbl'>Create table </button>
-    <button id = 'flipTbl'> Flip </button>
-    <button id = 'resetTableTranspose'> Reset </button>
-`);
+  function getTableTranspose(elem){
+      elem.insertAdjacentHTML('afterbegin', `
+        <input id = 'rowsTableTranspose' placeholder = 'int <= 15'><br>
+        <input id = 'columnsTableTranspose' placeholder = 'int <= 15'><br>
+        <div id = 'elemTableTranspose'>
+            <table id = 'transposeTbl' class = 'middle '></table></div><br>
+        <p id = 'errTableTranspose'></p>
+        <button id = 'buttCreateTbl'>Create table </button>
+        <button id = 'flipTbl'> Flip </button>
+        <button id = 'resetTableTranspose'> Reset </button>
+    `);
 
   } 
 
@@ -31,10 +35,7 @@ function startActionTranspose(){
   let buttReset = document.getElementById('resetTableTranspose');
   let rows = document.getElementById('rowsTableTranspose');
   let cols = document.getElementById('columnsTableTranspose');
-    let rowsChangeble;
-    let colsChangeble;
-    let inners;
-    let newWay ;
+  let rowsChangeble, colsChangeble, inners, newWay ;
 
 		buttCreateTable.addEventListener('click', function(){
        rowsChangeble = Number(rows.value);
@@ -70,78 +71,74 @@ function startActionTranspose(){
       tbl.innerHTML = '';
       buttCreateTable.disabled = false;
     });
-
-                  function changeWay(elemTbl, inpRowsElem, inpColsElem, innersVar, newWayVar){
-                    if(elemTbl.className == 'standartWay'){
-                      elemTbl.className = 'diffWay';
-                      let tempRows = Number(inpColsElem.value); 
-                      let tempCols = Number(inpRowsElem.value);
-                        createTbl(tempRows, tempCols, elemTbl);
-                      putTogether(innersVar, newWayVar);
-
-                    }else{
-                      elemTbl.className = 'standartWay';
-                      let tempRows = Number(inpRowsElem.value);
-                      let tempCols = Number(inpColsElem.value);
-                        createTbl(tempRows, tempCols, elemTbl);
-                      putTogether(innersVar);
-                    }
-                  }
-
-                  function createStartArr(rows, cols){
-                    let arr = [];
-                    let quantOfCells = rows * cols;
-                    for(let i = 1; i <= quantOfCells; i++){
-                      arr.push(i);
-                      }
-                      return arr;
-                  }
-
-
-                  function createTbl(rows,cols, par){
-                    for(let i = 0; i < rows; i++){
-                      let tr = createEl('tr', par);
-                        for(let j = 0; j < cols;  j++){
-                          let td = createEl('td', tr);
-                          td.className = 'cells';
-                        }
-                    }
-                  }
-
-                  function createNewWay(rows, cols){
-                    let tempArr = [];
-                    for(let i = 0; i < rows; i++){
-                      for(let j = i; j < cols * rows; j+=rows){
-                        tempArr.push(j);
-                      }
-                    }
-                    return tempArr;
-                  }
-
-                  function createEl(tag, par ){		
-                    let el = document.createElement(tag);	
-                      par.appendChild(el);
-                      return el;
-                  }		
-
-                  function putTogether(arr1, arr2){
-                    let elems = document.querySelectorAll('.cells')
-                    for(let i = 0; i < elems.length; i++){
-                      if(arr2 === undefined){
-                      elems[i].innerHTML = arr1[i];
-                      }else{
-                      elems[arr2[i]].innerHTML = arr1[i];
-                      }
-                    }
-
-                  }	
-
 }
 
+function changeWay(elemTbl, inpRowsElem, inpColsElem, innersVar, newWayVar){
+  if(elemTbl.className == 'standartWay'){
+    elemTbl.className = 'diffWay';
+    let tempRows = Number(inpColsElem.value); 
+    let tempCols = Number(inpRowsElem.value);
+      createTbl(tempRows, tempCols, elemTbl);
+    putTogether(innersVar, newWayVar);
 
-
-  if(transpose != null){
-      getTable();
-      startActionTranspose();
+  }else{
+    elemTbl.className = 'standartWay';
+    let tempRows = Number(inpRowsElem.value);
+    let tempCols = Number(inpColsElem.value);
+      createTbl(tempRows, tempCols, elemTbl);
+    putTogether(innersVar);
   }
 }
+
+function createStartArr(rows, cols){
+  let arr = [];
+  let quantOfCells = rows * cols;
+  for(let i = 1; i <= quantOfCells; i++){
+    arr.push(i);
+    }
+    return arr;
+}
+
+
+function createTbl(rows,cols, par){
+  for(let i = 0; i < rows; i++){
+    let tr = createEl('tr', par);
+      for(let j = 0; j < cols;  j++){
+        let td = createEl('td', tr);
+        td.className = 'cells';
+      }
+  }
+}
+
+function createNewWay(rows, cols){
+  let tempArr = [];
+  for(let i = 0; i < rows; i++){
+    for(let j = i; j < cols * rows; j+=rows){
+      tempArr.push(j);
+    }
+  }
+  return tempArr;
+}
+
+function createEl(tag, par ){		
+  let el = document.createElement(tag);	
+    par.appendChild(el);
+    return el;
+}		
+
+function putTogether(arr1, arr2){
+  let elems = document.querySelectorAll('.cells')
+  for(let i = 0; i < elems.length; i++){
+    if(arr2 === undefined){
+    elems[i].innerHTML = arr1[i];
+    }else{
+    elems[arr2[i]].innerHTML = arr1[i];
+    }
+  }
+
+}	
+
+
+
+
+
