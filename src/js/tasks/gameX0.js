88cd8c0abbe21e14631b483a90b9a2f2f094c4b1
zm412 <zm412412@gmail.com>
2020-09-18 16:46:47 -0400

    fixed gameX0

--- src/js/tasks/gameX0.js
+++ src/js/tasks/gameX0.js
@@ -11,136 +11,121 @@ module.exports = () =>{
 
 }
 
-  function getGame(elem){
-
-    elem.insertAdjacentHTML('afterbegin', `
-        <style>
-         #gameX0#tblGameX0{
-          position: absolute; 
-            top: 30%; 
-            left: 35%; 
-            width: 25%; 
-            height: 25%; 
-        }
-        .tdGameX0 {
-          width: 30px;
-          height: 25px;
-        }
-        table {
-          border: 1px solid black;
-          border-collapse: collapse;
-        }
-        td, th{
-          text-align: center;
-          border: 1px solid black;
-        }
-            
-            </style>
-            <table id = 'tblGameX0' class="middle"></table>
-              </div>
-              <div>	<p> Winner in this Game: <span id = 'who'></span></p>
-                  <button id = 'buttGameX0'>Start again</button>
-              </div>
-                  <div class = 'secBlock'>
-                        <p> Won X:<span id = 'winX'></span></p>
-                        <p> Won 0:<span id = 'win0'></span></p>
-                        <p> Draw:<span id = 'noWins'></span></p>
-                  </div>	
-          `);
-  }
+function getGame(elem){
+  elem.insertAdjacentHTML('afterbegin', `
+  <table id = 'tblGameX0' class="middle"></table>
+    </div>
+    <div>	<p> Winner in this Game: <span id = 'who'></span></p>
+    <button id = 'buttGameX0'>Start again</button>
+    </div>
+    <div class = 'secBlock'>
+      <p> Won X:<span id = 'winX'></span></p>
+      <p> Won 0:<span id = 'win0'></span></p>
+      <p> Draw:<span id = 'noWins'></span></p>
+    </div>	
+        `);
+}
 
   
 function startGame(res){
-  let tbl = document.getElementById('tblGameX0');
-  createTbl(tbl);
-  let who = document.getElementById('who');
-  let butt = document.getElementById('buttGameX0');
-  let tds = tbl.querySelectorAll('.tdGameX0');
-  let winX = document.getElementById('winX');
-  let win0 = document.getElementById('win0');
-  let noWins = document.getElementById('noWins');
-	let clicks = 0;
+  let domId = ['tblGameX0', 'who', 'winX', 'win0', 'noWins', 'buttGameX0'];
+  let {tblGameX0, who, winX, win0, noWins, buttGameX0} = difiningDOM(domId);
+    createTblForGameX0(tblGameX0);
+  let tds = tblGameX0.querySelectorAll('.tdGameX0');
+	let clicks = 0, gamer = 'X';
 	who.innerHTML = '';
-	let gamer = 'X';
-	let nextG = gamer => gamer == 'X' ? '0' : 'X';
-	addListener(tds, prepareCell);
-		
-		function prepareCell(){
-			this.innerHTML = gamer;
-			gamer = nextG(gamer);
-			this.removeEventListener('click',prepareCell);
-			clicks++;
+  createListener(tds, thisFunc, 'addEventListener');
+      function thisFunc(){
+       let winInfo = getWinner(tds, this, gamer, clicks) ;
+        if(winInfo.winner != undefined){
+          who.innerHTML = winInfo.winner;
+          createListener(tds, thisFunc, 'removeEventListener')
+        }else{
+          clicks = winInfo.clicks;
+          gamer = winInfo.gamer;
+          this.removeEventListener('click',thisFunc);
+      } }
+			document.querySelector('#buttGameX0').addEventListener('click', function(){
+			res.push(who.innerHTML);	
+      tblGameX0.innerHTML = '';
+			startGame(res);
+        collectResults(res, winX, win0, noWins);
+		});
+}
 
-				let winner = getWinner(tds);
-				if(winner === 'X'){
-					who.innerHTML = 'X';	
-				removeList(tds,prepareCell);
-				}
-				else if(winner === '0'){
-					who.innerHTML = '0'; 
-				removeList(tds, prepareCell);
-				}
-				else if(clicks > 8 && winner === undefined){
-					who.innerHTML = 'Drow';
-				}
-
+function difiningDOM(arr){
+  let obj = {};
+  for(let domId of arr){
+   obj[domId] = document.querySelector('#' + domId); 
+  }
+  return obj;
 }
 
 
-		butt.addEventListener('click', function(){
-			res.push(who.innerHTML);	
-      tbl.innerHTML = '';
-			startGame(res);
-        winX.innerHTML = res.reduce((sum, curr) => curr === 'X' ? ++sum  : sum, 0);
-				win0.innerHTML = res.reduce((sum, curr) => curr === '0' ? ++sum  : sum, 0);
-				noWins.innerHTML = res.reduce((sum, curr) => curr === 'Drow' ? ++sum  : sum, 0);
-		});
 
+function collectResults(resVar, elem1, elem2, elem3){
+  let individualCounter = (el, expr) => {
+  el.innerHTML = resVar.reduce((sum, curr) => curr === expr ? ++sum  : sum, 0);
   }
+    individualCounter(elem1, 'X');
+    individualCounter(elem2, '0');
+    individualCounter(elem3, 'Drow');
+}
 
-
-                    function getWinner(elems){
-                      let arr = [[0,1,2], [3,4,5], [6,7,8],[0,3,6],[1,4,7],[2,5,8], [0,4,8], [2,4,6]];
-                      let combsArr;
-                      let win;
-                        for(let i = 0; i < arr.length; i++){
-                          let comb = arr[i];
-                          combsArr = [elems[comb[0]].innerHTML, elems[comb[1]].innerHTML, elems[comb[2]].innerHTML]; 
-                          if(combsArr.every(elem => elem === 'X')){	win = 'X';}
-                          else if(combsArr.every(elem => elem === '0')){	win = '0'; }
-                    }
-                      return win;
-                    }
+function nextG(gamePlayer){
+ return gamePlayer == 'X' ? '0' : 'X';
+}
 
 
 
-                    function addListener(elems, func){
-                      for(let i = 0; i < elems.length; i++){
-                      elems[i].addEventListener('click', func);
-                      }
-                    }
+function getWinner(elems,elemThisHod, gamerVar, click){
+  let arr = [[0,1,2], [3,4,5], [6,7,8],[0,3,6],[1,4,7],[2,5,8], [0,4,8], [2,4,6]];
+    elemThisHod.innerHTML = gamerVar;
+    gamerVar = nextG(gamerVar);
+    click++;
+    let combsArr,  win;
+    for(let i = 0; i < arr.length; i++){
+      let comb = arr[i];
+      combsArr = [elems[comb[0]].innerHTML, elems[comb[1]].innerHTML, elems[comb[2]].innerHTML]; 
+      if(combsArr.every(elem => elem === 'X')){
+        win = 'X'; 
+      }else if(combsArr.every(elem => elem === '0')){	
+        win = '0'; 
+      } else if(click > 8 && win == undefined){
+       win = 'Drow'; 
+      }
+}
+      return {
+        gamer: gamerVar,
+        clicks: click,
+        winner: win
+      };
+}
 
 
-                    function removeList(elems, func){
-                      for(let i = 0; i < elems.length; i++){
-                      elems[i].removeEventListener('click', func);
-                      }
-                    }
-                     
-                    function createTbl(par){
-                      for(let i = 0; i < 3; i++){
-                        let tr = createEl('tr', par);
-                          for(let j = 0; j < 3; j++){
-                            let td = createEl('td', tr);
-                              td.className = 'tdGameX0';
-                          }
-                      }
-                    }
 
-                      function createEl(tag, par ){		
-                        let el = document.createElement(tag);	
-                          par.appendChild(el);
-                          return el;
-                      }		
+function createListener(elems, func, method){
+  for(let i = 0; i < elems.length; i++){
+  elems[i][method]('click', func);
+  }
+}
+
+
+ 
+function createTblForGameX0(par){
+  for(let i = 0; i < 3; i++){
+    let tr = createEl('tr', par);
+      for(let j = 0; j < 3; j++){
+        let td = createEl('td', tr);
+          td.className = 'tdGameX0';
+      }
+  }
+}
+
+  function createEl(tag, par ){		
+    let el = document.createElement(tag);	
+      par.appendChild(el);
+      return el;
+  }		
 
 
